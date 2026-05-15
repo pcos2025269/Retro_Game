52bcd5236fae4f635a9f3fb69f3eebbcfb377963
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-05-15 01:25:28 -0600

    feat: added function to create stars in background

--- SRC/JS/game.js
+++ SRC/JS/game.js
@@ -4,9 +4,21 @@ canvas .width = innerWidth -4 ;
 canvas.height = innerHeight -4 ;
 const ctx = canvas.getContext("2d");
 
+
+const stars = (number = 100) => {
+    for (let i = 0; i < number; i++) {
+        let x = Math.floor(Math.random() * canvas.width);
+        let y = Math.floor(Math.random() * canvas.height);
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, y, 2, 4);
+        ctx.fillRect(--x, ++y, 4, 2);
+    }
+};
+
 const drawBack = () => {
     ctx.fillStyle = "#323232";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    stars(25);
 }
 
 const update = () => {
