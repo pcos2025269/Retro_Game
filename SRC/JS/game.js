e174151c8826ab4bb77a94919dad7fd192522bd4
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-05-15 01:45:42 -0600

    feat: add class ship

--- SRC/JS/game.js
+++ SRC/JS/game.js
@@ -5,6 +5,34 @@ canvas.height = innerHeight -4 ;
 const ctx = canvas.getContext("2d");
 
 
+class Ship {
+    constructor() {
+
+        this.position = {
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+        }
+        this.w = 45;
+        this.h = 35;
+
+        
+        this.image = new Image();
+        this.image.src = "assets/ship.png";
+        this.image.onload = this.draw;
+        this.angle = 0;
+    }
+
+    draw() {
+        if (!this.image) return;
+        ctx.save();
+        ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+        ctx.restore();
+    }
+
+}
+
 const stars = (number = 100) => {
     for (let i = 0; i < number; i++) {
         let x = Math.floor(Math.random() * canvas.width);
@@ -24,8 +52,11 @@ const drawBack = () => {
 const update = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBack();
+    drawShip();
     requestAnimationFrame(update);
 }
+
+
 
 
 update();
