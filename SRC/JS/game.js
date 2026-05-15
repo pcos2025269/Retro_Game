d6d4551a8900649935b31ef9f089637ef84e3a8e
pcos-2025269 <pcos-2025269@kinal.edu.gt>
2026-05-15 11:02:42 -0600

    feat: added original movement to a ship

--- SRC/JS/game.js
+++ SRC/JS/game.js
@@ -4,6 +4,7 @@ canvas .width = innerWidth -4 ;
 canvas.height = innerHeight -4 ;
 const ctx = canvas.getContext("2d");
 
+const KEYS = {};
 
 class Ship {
     constructor() {
@@ -14,7 +15,11 @@ class Ship {
         }
         this.w = 45;
         this.h = 35;
-
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+        this.friction = 0.80;
         
         this.image = new Image();
         this.image.src = "SRC/IMG/ship.png";
@@ -22,41 +27,127 @@ class Ship {
         this.angle = 0;
     }
 
-    draw() {
-
-        if (!this.image) return;
-
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.w, this.h);
+    Borders() {
+        //bordes derecha e izquierda
+        if (this.position.x < 0) {
+            this.position.x = canvas.width-this.w
+        } else if (this.position.x  > canvas.width-this.w) {
+            this.position.x = 0
+            
+        }
+        //bordes de arriba y abajo
+        if (this.position.y < 0) {
+            this.position.y = canvas.height-this.h
+        } else if (this.position.y  > canvas.height-this.h) {
+            this.position.y = 0
+        }
     }
 
+    move() {
+        if (KEYS["ArrowLeft"]) {
+            this.angle -= 0.08
+        }
+
+        if (KEYS["ArrowRight"]) {
+            this.angle += 0.08
+        }
+
+        // acelerar hacia delante
+        if (KEYS["ArrowUp"]) {
+
+            this.velocity.x += Math.cos(this.angle - Math.PI / 2) * 0.4
+
+            this.velocity.y += Math.sin(this.angle - Math.PI / 2) * 0.4
+        }
+
+        // retroceder
+        if (KEYS["ArrowDown"]) {
+
+            this.velocity.x -= Math.cos(this.angle - Math.PI / 2) * 0.2
+
+            this.velocity.y -= Math.sin(this.angle - Math.PI / 2) * 0.2
+        }
+
+        // mover
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        // fricción
+        this.velocity.x *= this.friction
+        this.velocity.y *= this.friction
+
+        this.Borders()
+    }
+
+    update() {
+        this.draw()
+        this.move()
+    }
+
+    draw() {
+        if (!this.image.complete) return
+
+        if (!this.image.complete) return
+
+        ctx.save()
+
+        // centro de la nave
+        ctx.translate(
+            this.position.x + this.w / 2,
+            this.position.y + this.h / 2
+        )
+
+        // rotar
+        ctx.rotate(this.angle)
+
+        // dibujar centrado
+        ctx.drawImage(
+            this.image,
+            -this.w / 2,
+            -this.h / 2,
+            this.w,
+            this.h
+        )
+
+        ctx.restore()
+    }
 }
 
 const ship = new Ship();
 
 const stars = (number = 100) => {
     for (let i = 0; i < number; i++) {
-        let x = Math.floor(Math.random() * canvas.width);
-        let y = Math.floor(Math.random() * canvas.height);
-        ctx.fillStyle = "white";
-        ctx.fillRect(x, y, 2, 4);
-        ctx.fillRect(--x, ++y, 4, 2);
+        let x = Math.floor(Math.random() * canvas.width)
+        let y = Math.floor(Math.random() * canvas.height)
+        ctx.fillStyle = "white"
+        ctx.fillRect(x, y, 2, 4)
+        ctx.fillRect(--x, ++y, 4, 2)
     }
 };
 
 const drawBack = () => {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    stars(25);
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    stars(25)
 }
 
 const update = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBack();
-    ship.draw();
-    requestAnimationFrame(update);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawBack()
+   
+    ship.update()
+    requestAnimationFrame(update)
 }
 
+addEventListener("keydown", e => {
+    console.log(e.key)
+    KEYS[e.key] = true
+});
+
+addEventListener("keyup", e => {
+    console.log(e.key)
+    KEYS[e.key] = false
+}); 
 
 
-
-update();
+update()
